2423b3d424d3838bff205d1a0a60a2333f32428b
Elias Little <eliasjlittle@gmail.com>
2024-11-19 22:09:08 -0500

    Updated strategy for better demonstration, and added main function

--- TestStrategy.py
+++ TestStrategy.py
@@ -3,25 +3,39 @@ from MarketMonitor import monitor_event
 from Series import event_list
 from OrderBook import OrderBook
 from OrderManagementSystem import OrderManagementSystem, Order
-from KalshiAPI import demo_kalshi_api
+from KalshiAPI import kalshi_api
 
-oms = OrderManagementSystem(demo_kalshi_api)
+oms = OrderManagementSystem(kalshi_api)
 
-# If the best no price is between 50 and 70, send a buy order for 10 contracts at (at most) the best no price + 5 cents
+# If the best yes price is between 5 and 20 for any orderbook in this event, 
+# send a buy order for 10 no-contracts at (at most) the best yes price + 5 cents
+#
 # WARNING: This strategy is not profitable and is only for demonstration purposes
-def strategy(orderbook: OrderBook, updates: int, timestamp: float, logger: logging.Logger):
-    if orderbook.best_no > 50 and orderbook.best_no < 70:
-        oms.send_order(Order(
-            ticker=orderbook.ticker,
-            action="buy",
-            type="limit",
-            no_price=orderbook.best_no + 5,
-            amount=10,
-            side="no"
-        ))
+class TestStrategy:
+    def __init__(self, logger: logging.Logger):
+        self.logger = logger
 
-    logger.info(f"Orderbook: {orderbook}")
+    def update(self, orderbook: OrderBook, updates: int, timestamp: float):
+        best_yes = orderbook.best_yes # Check if there is a best yes
 
-def main(logger: logging.Logger):
-    event = event_list[0]
-    monitor_event(event, strategy, logger)
+        if best_yes and best_yes.price > 5 and best_yes.price < 20:
+            print(f"Sending buy order for {orderbook.ticker}:no at limit price {best_yes.price + 5} for 10 contracts")
+            self.logger.info(f"Sending buy order for {orderbook.ticker}:no at limit price {orderbook.best_yes.price + 5} for 10 contracts")
+
+            oms.send_order(Order(
+                ticker=orderbook.ticker,
+                action="buy",
+                type="limit",
+                no_price=orderbook.best_yes.price + 5,
+                amount=10,
+                side="no"
+            ))
+
+async def main(logger: logging.Logger, args):
+    if args.ticker:
+        event = args.ticker
+    else:
+        event = event_list[0]
+
+    logger.info(f"Starting test strategy with event: {event}")
+    await monitor_event(event, TestStrategy(logger).update, logger)
